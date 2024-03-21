9010f371a4c00e7a166e4a61b2ad948a61b6d0d8
Poorna <poorna029@gmail.com>
2024-03-21 20:27:28 +0530

    commit2

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
+const fs = require("fs");
+const csv = require("csv-parser");
 
 const app = express();
 
@@ -26,14 +28,32 @@ db.connect((err) => {
 
 // Define routes
 app.get("/search", (req, res) => {
-  const { ticker, revenue, gp, period } = req.query;
+  const { ticker, revenue, gp, period, order_by } = req.query;
   const periodInYears = period ? period.slice(0, 1) : 0;
-  const get_search_qry = `select * from businessquant_info where ticker like '${
+  const givenDate = new Date();
+  givenDate.setFullYear(givenDate.getFullYear() - periodInYears);
+  const month = givenDate.getMonth() + 1; // Month is zero-based, so add 1
+  const day = givenDate.getDate();
+  const year = givenDate.getFullYear();
+  const req_date = `${year}-${month}-${day}`;
+  //   console.log(req_date);
+
+  const get_search_qry = `select * from businessquant_data where ticker like '${
     ticker || "%%"
-  }'and revenue>=${revenue || "revenue"} and gp>=${
-    Array.isArray(gp) ? gp[0] : 0
-  } and date<=DATE_SUB(NOW(), INTERVAL ${periodInYears} YEAR);`;
-  console.log(get_search_qry, "poorna search qry is printing");
+  }' 
+  and revenue>=${revenue || 0} and
+   gp>=${Array.isArray(gp) ? gp[0] : 0} 
+  and 
+  ${
+    periodInYears
+      ? `YEAR(date) >=${year} and (YEAR(date) >= ${year} and MONTH(date) >= ${month})OR (YEAR(date) >=${year}  AND MONTH(date) >=${month} AND DAY(date) >=${day})`
+      : `date=date`
+  }  ${
+    order_by
+      ? `order by ${order_by}`
+      : "order by ticker, date, revenue desc,gp desc "
+  }; `;
+  //   console.log(get_search_qry, "poorna search qry is printing");
 
   db.query(get_search_qry, (err, results) => {
     if (err) {
@@ -45,26 +65,24 @@ app.get("/search", (req, res) => {
   });
 });
 
-// // Route to fetch data from MySQL
-// app.get("/insert/", async (req, res) => {
-//   console.log("poo");
-//   try {
-//     const get_search_qry = `select * from businessquant_info where ticker like '${
-//       ticker || "%%"
-//     }'and revenue>=${revenue || "revenue"} and gp>=${
-//       Array.isArray(gp) ? gp[0] : 0
-//     } and date<=DATE_SUB(NOW(), INTERVAL ${periodInYears} YEAR);`;
-//     console.log(get_search_qry, "poorna search qry is printing");
+const data = [];
 
-//     const response = await connection.query(get_search_qry);
-//     console.log(response, "response");
+// Path to your CSV file
+const csvFilePath = "data.csv";
 
-//     // res.json(rows);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send("Server Error");
-//   }
-// });
+// Read the CSV file and parse its contents
+fs.createReadStream("./Sample-Data-Historic.csv")
+  .pipe(csv())
+  .on("data", (row) => {
+    // Process each row of CSV data
+    const a = Object.values(row);
+    data.push({ ...row, ticker: a[0] });
+  })
+  .on("end", () => {
+    // All rows have been processed
+    // create_rows(data);
+    console.log("qry insertion started at :", new Date().toLocaleTimeString());
+  });
 
 // Start the Express server
 const PORT = process.env.PORT || 3000;
@@ -72,3 +90,30 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
+
+function create_rows(aar) {
+  aar.forEach((element, ind) => {
+    value = ind;
+    const { ticker, date, revenue, gp, fcf, capex } = element;
+    // console.log(ticker, date, revenue, gp, fcf, capex);
+    const newdate = date.split("/");
+    const date_format = `${newdate[2]}-${newdate[0]}-${newdate[1]}`; // YYYY-MM-DD
+
+    const qry = `insert into businessquant_data(ticker,date,revenue,gp,fcf,capex) values('${ticker}','${date_format}',${
+      revenue || 0
+    },${gp || 0},${fcf || 0},${capex || 0} );`;
+
+    console.log(qry);
+
+    db.query(qry, (err) => {
+      if (err) {
+        console.error("Error fetching users:", err);
+        // res.status(500);
+        // res.send("Error fetching users");
+        throw new Error("Something went wrong!");
+        return;
+      }
+    });
+  });
+  console.log("qry insertion finised at :", new Date().toLocaleTimeString());
+}
